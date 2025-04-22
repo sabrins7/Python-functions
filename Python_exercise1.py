2138be9e06cf19a6d4b2b62047a0abe7ead8550d
Sabrin_k <j_sabrin7@yahoo.com>
2025-04-22 13:34:50 +0300

    Rename file; typo corrected to Python_exercise1.py

---
+++ Python_exercise1.py
@@ -0,0 +1,17 @@
+sentence = input ("Enter a sentence please: ")
+sentence_length = len(sentence)
+print(f"The length of the sentence is : {sentence_length}.")
+
+#variable that stores your favorite color
+my_favorite_color = input("what is your favorite color?")
+print(f"your favorite color is: {my_favorite_color}.")
+# variable that stores your age
+my_age =input("What is your age?") 
+print(f"My age is: {my_age}.")
+# variable that stores the name of your favorite animal.
+my_favorite_animal = input("What is your favorite animal?")
+print(f"My favorite animal is: {my_favorite_animal}")
+
+
+#The names I chose for the variables are valid and appropriate because 
+# they are simple & clear & according to Python's guidelines.
